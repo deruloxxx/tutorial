75009369191abb72c2b0b1245f416aa789a432e5
ryuyauetake <ryuya@anymindgroup.com>
2021-10-10 15:14:49 +0900

    Feat test

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,15 @@
 class Blog < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :body, presence: true, length: { maximum: 10_000 }
+  validate :proper_title_and_body
+
+  private
+    def proper_title_and_body
+      unless title.starts_with?('今日')
+        errors.add(:title, 'は「今日」から初めてください。')
+      end
+      unless body.ends_with?('。')
+        errors.add(:body, 'は「。」で終了してください。')
+      end
+    end
 end

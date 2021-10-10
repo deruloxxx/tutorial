9100b43c838a4bfff1342ddc73d79ad857f1f670
ryuyauetake <ryuya@anymindgroup.com>
2021-10-10 11:33:16 +0900

    Feat scaffold blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_09_152342) do
+ActiveRecord::Schema.define(version: 2021_10_10_022757) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.string "string"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "diaries", force: :cascade do |t|
     t.string "title"
+    t.string "string"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
